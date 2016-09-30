96c1cbc2492cb32a7857af6bfd1e5617bc040beb
Kevin McAnoy <kmcanoy@redhat.com>
2016-09-30 09:07:13 -0700

    Remove unused code

--- src/main/java/com/rhc/automation/Swagger2SpringBoot.java
+++ src/main/java/com/rhc/automation/Swagger2SpringBoot.java
@@ -1,17 +1,10 @@
 package com.rhc.automation;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
-import com.rhc.automation.service.CustomerService;
-import com.rhc.automation.service.OpenShiftService;
-import com.rhc.automation.service.UserService;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -19,7 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @ComponentScan(basePackages = "com.rhc.automation")
 public class Swagger2SpringBoot implements CommandLineRunner {
-    private static final Logger log = LoggerFactory.getLogger(Swagger2SpringBoot.class);
 
     @Override
     public void run(String... arg0) throws Exception {
@@ -30,14 +22,6 @@ public class Swagger2SpringBoot implements CommandLineRunner {
 
     public static void main(String[] args) throws Exception {
         new SpringApplication(Swagger2SpringBoot.class).run(args);
-    }
-
-    @Bean
-    public CommandLineRunner startUp(UserService userService, OpenShiftService openShiftService,
-            CustomerService customerService) {
-        return (args) -> {
-            
-        };
     }
 
     class ExitException extends RuntimeException implements ExitCodeGenerator {
